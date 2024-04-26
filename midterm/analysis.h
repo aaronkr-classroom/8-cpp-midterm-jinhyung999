32fee10b5e52dfe555c0d085921d9854ac94ab8f
jinhyung999 <jinju8640108@naver.com>
2024-04-26 11:17:41 +0900

    midterm

---
+++ midterm/analysis.h
@@ -0,0 +1,22 @@
+#pragma once
+#ifndef GUARD_analysis_h
+#define GUARD_analysis_h
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include "Student_info.h"
+
+double average(const vector<double>&);//
+double optimistic_median(const Student_info&);//
+double optimistic_median_analysis(const std::vector<Student_info>&);//
+double grade_aux(const Student_info&);//
+double median_analysis(const std::vector<Student_info>&);//
+double average_grade(const Student_info&);//
+double average_analysis(const std::vector<Student_info>&);//
+void write_analysis(std::ostream&, const std::string&,
+    double analysis(const std::vector<Student_info>&),
+    const std::vector<Student_info>&,
+    const std::vector<Student_info>&);//
+bool did_all_hw(const Student_info&);//
+#endif
